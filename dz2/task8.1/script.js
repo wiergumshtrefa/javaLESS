bbdffbf6a4fb0366e87e2b1858f265fbe1039ab2
wiergushtrefa <colbert_theht@bk.ru>
2024-12-19 21:27:27 +0300

    aaaaaaa

---
+++ dz2/task8.1/script.js
@@ -0,0 +1,4 @@
+document.getElementById('discountButton').addEventListener('click', function() {
+    const circle = Number(document.getElementById('circleInput').vaule;
+    const square = Number(document.getElementById('suareInput').vaule;
+const circle
